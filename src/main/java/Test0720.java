104442e10b1be36b7e99eb2bd032c7ba9e01badf
ywc <ywc@123.com>
2020-11-27 20:50:52 +0800

    hot-fix commit

--- src/main/java/Test0720.java
+++ src/main/java/Test0720.java
@@ -6,5 +6,6 @@ public class Test0720 {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("test");
+        System.out.println("hot-fix");
     }
 }
