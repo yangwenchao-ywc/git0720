802dd61f3a343705f3c47144af8318af1654c2c8
yangwenchao-ywc <75104706+yangwenchao-ywc@users.noreply.github.com>
2020-11-27 21:12:24 +0800

    Update Test0720.java

--- src/main/java/Test0720.java
+++ src/main/java/Test0720.java
@@ -8,5 +8,6 @@ public class Test0720 {
         System.out.println("test");
         System.out.println("hot-fix");
         System.out.println("test");
+        System.out.println("change");
     }
 }
